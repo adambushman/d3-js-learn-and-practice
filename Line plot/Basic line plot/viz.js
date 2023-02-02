61cd4629755d289b65c43ee4197f978fbf71d4fd
adambushman <adambushman@mail.weber.edu>
2023-02-02 13:29:39 -0700

    Added titles and labels

--- Line plot/Basic line plot/viz.js
+++ Line plot/Basic line plot/viz.js
@@ -6,6 +6,12 @@ let margin = {top: s * 0.2, right: s * 0.05, bottom: s * 0.12, left: s * 0.17};
 let width = (s * 1.5) - margin.left - margin.right;
 let height = s - margin.top - margin.bottom;
 
+// Defining the titles and labels
+
+let title = "Daily Site Visits Trend";
+let subtitle = "Fake, Randomly Generated Site Visit Data"
+let ylab = "Daily Site Visits"
+
 // Defining the scales
 
 let xScale
@@ -68,7 +74,34 @@ d3.csv('../../Data files/site_visits.csv',
             .attr("d", d3.line()
                 .x((d) => { return xScale(d.date) })
                 .y((d) => { return yScale(d.site_visits) })
-            )
+            );
+
+        // Adding the titles and labels
+        
+        svg.append("text")
+            .attr("x", (width / 2) - (margin.left / 2))
+            .attr("y", margin.top / -2)
+            .style("font-size", 30)
+            .style("font-weight", 600)
+            .style("text-anchor", "middle")
+            .style("fill", "#F7F7FF")
+            .text(title);
+
+        svg.append("text")
+            .attr("x", (width / 2) - (margin.left / 2))
+            .attr("y", margin.top / -4)
+            .style("font-size", 16)
+            .style("text-anchor", "middle")
+            .text(subtitle);
+
+        svg.append("text")
+            .attr("x", (margin.left / -2))
+            .attr("y", height / 2)
+            .attr('transform', 'rotate(-90, ' + (margin.left  * 2 / -3) + ', ' + (height / 2) + ')')
+            .style("font-size", 16)
+            .style("font-weight", 600)
+            .style("text-anchor", "middle")
+            .text(ylab);
 
     }
     
