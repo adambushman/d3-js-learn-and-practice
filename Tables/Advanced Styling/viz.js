ec783b6a1b8f5bfab104fd8c1c09640e86d408fe
adambushman <adambushman@mail.weber.edu>
2023-03-10 11:02:23 -0700

    Adjustments for 4 (four) styling examples

--- Tables/Advanced Styling/viz.js
+++ Tables/Advanced Styling/viz.js
@@ -1,46 +1,48 @@
 let columns
 
-d3.csv("../../Data files/nba_efficiency.csv", (data) => {
+function renderTable(table_name) {
+    d3.csv("../../Data files/nba_efficiency.csv", (data) => {
 
-    // Getting the column names
+        // Getting the column names
+    
+         data.forEach((d) => {
+            columns = Object.keys(d)
+        });
+    
+        // Creating the table
+    
+        let table = d3.select(table_name)
+            .append("table");
+    
+        // Setting up the table head and body
+    
+        let thead = table.append("thead");
+    
+        let tbody = table.append("tbody");
+    
+        // Adding the column headers
+    
+        thead.append("tr")
+            .selectAll("th")
+            .data(columns)
+            .enter()
+            .append("th")
+            .text((c) => { return c });
+    
+        tbody.selectAll("tr")
+            .data(data)
+            .enter()
+            .append("tr")
+            .selectAll("td")
+            .data((d) => { return d3.entries(d) })
+            .enter()
+            .append("td")
+            .text((d) => { return d.value });
+            
+    })
+}
 
-     data.forEach((d) => {
-        columns = Object.keys(d)
-    });
-
-    // Creating the table
-
-    let table = d3.select("#my-table")
-        .append("table");
-
-    // Creating a title
-
-    let caption = table.append("caption")
-        .text("Top NBA Scorers")
-
-    // Setting up the table head and body
-
-    let thead = table.append("thead");
-
-    let tbody = table.append("tbody");
-
-    // Adding the column headers
-
-    thead.append("tr")
-        .selectAll("th")
-        .data(columns)
-        .enter()
-        .append("th")
-        .text((c) => { return c });
-
-    tbody.selectAll("tr")
-        .data(data)
-        .enter()
-        .append("tr")
-        .selectAll("td")
-        .data((d) => { return d3.entries(d) })
-        .enter()
-        .append("td")
-        .text((d) => { return d.value });
-        
-})
+renderTable("#my-table-1")
+renderTable("#my-table-2")
+renderTable("#my-table-3")
+renderTable("#my-table-4")
