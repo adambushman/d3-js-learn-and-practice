6e4ecdd481ba706ba6a2fdbdf46cadb2e2ce0103
adambushman <adambushman@mail.weber.edu>
2023-09-22 17:29:54 -0600

    Added text wtrap function

--- Tile Map/script.js
+++ Tile Map/script.js
@@ -64,6 +64,82 @@ function getColor(data, metric, d) {
     return(colorScale(d));
 }
 
+// Custom wrapping function
+
+const wrapText = (
+        text,
+        width,
+        dyAdjust = -0.15,
+        lineHeightEms = 1.05,
+        lineHeightSquishFactor = 1,
+        splitOnSlash = true,
+        centreVertically = true
+    ) => {
+        text.each(function () {
+          var text = d3.select(this),
+            x = text.attr("x"),
+            y = text.attr("y");
+      
+          var words = [];
+          text
+            .text()
+            .split(/\s+/)
+            .forEach(function (w) {
+              if (splitOnSlash) {
+                var subWords = w.split("/");
+                for (var i = 0; i < subWords.length - 1; i++)
+                  words.push(subWords[i] + "/");
+                words.push(subWords[subWords.length - 1] + " ");
+              } else {
+                words.push(w + " ");
+              }
+            });
+      
+          text.text(null); // Empty the text element
+      
+          // `tspan` is the tspan element that is currently being added to
+          var tspan = text.append("tspan");
+      
+          var line = ""; // The current value of the line
+          var prevLine = ""; // The value of the line before the last word (or sub-word) was added
+          var nWordsInLine = 0; // Number of words in the line
+          for (var i = 0; i < words.length; i++) {
+            var word = words[i];
+            prevLine = line;
+            line = line + word;
+            ++nWordsInLine;
+            tspan.text(line.trim());
+            if (tspan.node().getComputedTextLength() > width && nWordsInLine > 1) {
+              // The tspan is too long, and it contains more than one word.
+              // Remove the last word and add it to a new tspan.
+              tspan.text(prevLine.trim());
+              prevLine = "";
+              line = word;
+              nWordsInLine = 1;
+              tspan = text.append("tspan").text(word.trim());
+            }
+          }
+      
+          var tspans = text.selectAll("tspan");
+      
+          var h = lineHeightEms;
+          // Reduce the line height a bit if there are more than 2 lines.
+          if (tspans.size() > 2)
+            for (var i = 0; i < tspans.size(); i++) h *= lineHeightSquishFactor;
+      
+          tspans.each(function (d, i) {
+            // Calculate the y offset (dy) for each tspan so that the vertical centre
+            // of the tspans roughly aligns with the text element's y position.
+            var dy = i * h + dyAdjust;
+            if (centreVertically) dy -= ((tspans.size() - 1) * h) / 2;
+            d3.select(this)
+              .attr("y", y)
+              .attr("x", x)
+              .attr("dy", dy + "em");
+          });
+        });
+    }
+
 // Getting tooltip position, accounting for dynamic screens
 
 function tooltipPosition(val_x, val_y) {
