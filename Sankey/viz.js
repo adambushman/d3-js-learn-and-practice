33b162b4c471abd00adc04ba6f8dfbb7bb063786
adambushman <adambushman@mail.weber.edu>
2023-09-30 17:55:56 -0600

    Fixed issue with highlighting a node's links

--- Sankey/viz.js
+++ Sankey/viz.js
@@ -8,6 +8,40 @@ const dims = {
     width: 1200 - margins.left - margins.right
 }
 
+const mouseover = (d) => {
+    const name = d.name.replace(" ", "-");
+    const rect = d3.select(`.${name} .rect`);
+    let links;
+
+    if(["Center", "Power Forward", "Small Forward", "Point Guard", "Shooting Guard"].includes(d.name)) {
+        links = d3.selectAll(`.source-${name}`);
+    } else{
+        links = d3.selectAll(`.target-${name}`);
+    }
+
+    rect.transition().duration(500).style("opacity", 1);
+    links.transition().duration(500).style("opacity", 1);
+}
+
+const mousemove = (d) => {
+
+}
+
+const mouseleave = (d) => {
+    const name = d.name.replace(" ", "-");
+    const rect = d3.select(`.${name} .rect`);
+    let links;
+
+    if(["Center", "Power Forward", "Small Forward", "Point Guard", "Shooting Guard"].includes(d.name)) {
+        links = d3.selectAll(`.source-${name}`);
+    } else{
+        links = d3.selectAll(`.target-${name}`);
+    }
+
+    rect.transition().duration(500).style("opacity", 0.7);
+    links.transition().duration(500).style("opacity", 0.35);
+}
+
 
 let svg = d3.select("#sankey-plot")
     .append("svg")
@@ -36,7 +70,11 @@ d3.json("/Data files/shot-charts.json", function(error, graph) {
         .data(graph.links)
         .enter()
         .append("path")
-                .attr("class", "link")
+                .attr("class", d => {
+                    const source = d.source.name.replace(" ", "-");
+                    const target = d.target.name.replace(" ", "-");
+                    return `source-${source} target-${target} link`;
+                })
                 .attr("d", sankey.link() )
                 .style("stroke-width", function(d) { return Math.max(1, d.dy); })
                 .sort(function(a, b) { return b.dy - a.dy; });
@@ -46,17 +84,27 @@ d3.json("/Data files/shot-charts.json", function(error, graph) {
         .selectAll(".node")
         .data(graph.nodes)
         .enter().append("g")
-                .attr("class", "node")
+                .attr("class", d => {
+                    const name = d.name.replace(" ", "-");
+                    return `${name} node`;
+                })
                 .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
         
     // add the rectangles for the nodes
     node
         .append("rect")
+            .attr("class", d => {
+                const name = d.name.replace(" ", "-");
+                return `${name} rect`;
+            })
             .attr("height", function(d) { return d.dy; })
             .attr("width", sankey.nodeWidth())
             //.style("fill", function(d) { return d.color = color(d.name.replace(/ .*/, "")); })
             //.style("stroke", function(d) { return d3.rgb(d.color).darker(2); })
         // Add hover text
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave)
         .append("title")
             .text(function(d) { return d.name + "\n" + "There is " + d.value + " stuff in this node"; });
     
