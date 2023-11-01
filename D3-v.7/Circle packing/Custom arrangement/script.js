7eda344127db1e2034fadd7cf577e261883e7c1a
adambushman <adam.bushman1@gmail.com>
2023-11-01 09:45:03 -0600

    Custom arranged circle packing concept

---
+++ D3-v.7/Circle packing/Custom arrangement/script.js
@@ -0,0 +1,106 @@
+const dims = {
+    width: 500, height: 350, 
+    top: 10, left: 10, right: 10, bottom: 10
+}
+
+let [red, yellow, green, blue] = ["red", "yellow", "green", "blue"];
+
+const svg = d3.select("#chart")
+    .append("svg")
+    .attr("id", "chart-svg")
+    .attr("viewBox", `0 0 ${dims.width} ${dims.height}`)
+    .append("g")
+    .attr("transform", `translate(${dims.left},${dims.top})`);
+
+const data = [
+    {x: 50, y: 50, label: "Sunnyside", points: [
+        {size: 5, color: red},
+        {size: 7, color: yellow},
+        {size: 9, color: green},
+        {size: 11, color: blue},
+    ]},
+    {x: 150, y: 125, label: "Carlsbad", points: [
+        {size: 3, color: red},
+        {size: 8, color: yellow},
+        {size: 2, color: green},
+        {size: 6, color: blue},
+    ]},
+    {x: 250, y: 200, label: "Camareo", points: [
+        {size: 3, color: red},
+        {size: 4, color: yellow},
+        {size: 5, color: green},
+        {size: 1, color: blue},
+    ]},
+    {x: 350, y: 275, label: "Oceanview", points: [
+        {size: 6, color: red},
+        {size: 6, color: yellow},
+        {size: 3, color: green},
+        {size: 4, color: blue},
+    ]}
+];
+
+svg.append("g")
+    .attr("class", "points")
+    .selectAll()
+    .data(data)
+    .join(
+        (enter) => {
+            enter.append("circle")
+                .attr("r", 30)
+                .style("stroke", "gray")
+                .style("stroke-width", 1)
+                .style("fill", "white")
+                .attr("cx", d => d.x)
+                .attr("cy", d => d.y);
+
+            enter.append("text")
+                .style("text-anchor", "middle")
+                .style("font-size", "0.5rem")
+                .attr("x", d => d.x)
+                .attr("y", d => d.y - 35)
+                .text(d => d.label);
+
+            const pack = d3.pack().size([60,60]).padding(2);
+
+            const circle_pack = enter.append("g")
+                .attr("id", d => d.label)
+                .attr("class", "packed-circle")
+                .style("fill-opacity", 0.6)
+                .attr("transform", d => `translate(${d.x - 30},${d.y - 30})`)
+                .on("mouseover", mouseover)
+                .on("mouseleave", mouseleave);
+            
+            const nodes = circle_pack.selectAll()
+                .data(d => pack(d3.hierarchy({children: d.points}).sum(dd => dd.size)).leaves())
+                .join("g")
+                .attr("transform", dd => `translate(${dd.x},${dd.y})`);
+
+            nodes.append("circle")
+                .style("stroke", "gray")
+                .style("stroke-width", 0.5)
+                .attr("r", dd => dd.r)
+                .style("fill", dd => dd.data.color);
+
+        }, 
+        (update) => update, 
+        (exit) => exit.remove()
+    )
+
+function mouseover(d) {
+    const target = d.target.parentElement.parentElement;
+    
+    d3.select(target)
+        .transition().duration(250)
+        .style("fill-opacity", 0.9);
+
+    d3.selectAll(".packed-circle")
+        .filter(c => c.label != target.id)
+        .transition().duration(250)
+        .style("fill-opacity", 0.3);
+}
+
+function mouseleave(d) {
+    d3.selectAll(".packed-circle")
+        .transition().duration(250)
+        .style("fill-opacity", 0.6);
+}
