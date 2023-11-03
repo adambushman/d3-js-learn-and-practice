ba4f934a6a652588cabb010c04fc7afd7622cb70
adambushman <adambushman@mail.weber.edu>
2023-11-02 21:29:17 -0600

    Packed circle tile arrangement

---
+++ D3-v.7/Circle packing/NBA Shots/script.js
@@ -0,0 +1,225 @@
+// Global variables
+const state = {
+    shapes_data: [], 
+    shooting_data: [], 
+    dims: {
+        width: 500, height: 500, 
+        top: 10, left: 10, right: 10, bottom: 10
+    }, 
+    locations: ["Rim", "Midrange", "Corner3", "AboveBreak3"]
+}
+
+function mouseover(d) {
+    const target = d.target.parentElement.parentElement;
+    console.dir(target);
+    d3.select(target)
+        .transition().duration(250)
+        .style("fill-opacity", 0.9);
+
+    d3.selectAll(".packed-circle")
+        .filter(c => c.Abb != target.id)
+        .transition().duration(250)
+        .style("fill-opacity", 0.3);
+}
+
+function mouseleave(d) {
+    d3.selectAll(".packed-circle")
+        .transition().duration(250)
+        .style("fill-opacity", 0.6);
+}
+
+// Wrangle data based on structure
+function wrangleData() {
+    const cleaned = aq.from(state.shapes_data)
+        .groupby(["team_abbreviation", "center_x", "center_y"])
+        .rollup({count: d => op.count()})
+        .join(
+            aq.from(state.shooting_data), 
+            ["team_abbreviation", "Abb"]
+        )
+        .fold(
+            ["Rim", "Midrange", "Corner3", "AboveBreak3"], 
+            {as: ['Location', 'Value']}
+        )
+        .derive({
+            Value: aq.escape(d => parseFloat(d.Value)), 
+            center_x: aq.escape(d => parseInt(d.center_x)), 
+            center_y: aq.escape(d => parseInt(d.center_y))
+        })
+        .groupby(['Abb', 'FullName'])
+        .derive({Total: d => op.sum(d.Value)})
+        .derive({Rate: d => d.Value / d.Total})
+        .select(["Abb", "FullName", "Location", "Rate", 'center_x', 'center_y'])
+        .groupby(['Abb', 'FullName', 'center_x', 'center_y'])
+        .rollup({data: d => op.array_agg({val: d.Rate, loc: d.Location})})
+        .objects();
+    
+    return cleaned;
+}
+
+// Filter the raw data according to user selection
+function filterData(filtered) {
+    return filtered;
+}
+
+// Visualization logic
+function createVis() {
+    // Setup vis structure: SVG, scales, etc.
+    const svg = d3.select("#viz")
+        .append("svg")
+        .attr("id", "viz-svg")
+        .attr("viewBox", `0 0 ${state.dims.width} ${state.dims.height}`)
+        .append("g")
+        .attr("transform", `translate(${state.dims.left},${state.dims.top})`);
+
+    const colorScale = d3.scaleOrdinal()
+        .domain(["Rim", "Midrange", "Corner3", "AboveBreak3"])
+        .range(["#ECC30B", "#3C91E6", "#FF7F11", "#8FC93A"]);
+
+    const legend = svg.append("g")
+        .attr("class", "legend");
+
+    const xScale = d3.scaleLinear();
+        
+    const yScale = d3.scaleLinear();
+    
+    legend.selectAll()
+        .data(state.locations)
+        .join(
+            (enter) => {
+                enter.append("circle")
+                    .attr("cx", 25)
+                    .attr("cy", (d,i) => state.dims.height - ((i+1) * 25) - 50)
+                    .style("fill", (d) => colorScale(d))
+                    .style("stroke", "gray")
+                    .style("stroke-width", 0.5)
+                    .style("fill-opacity", 0.5)
+                    .transition().duration(750)
+                    .attr("r", 10);
+
+                enter.append("text")
+                    .attr("x", 38)
+                    .attr("y", (d,i) => state.dims.height - ((i+1) * 25) - 46)
+                    .style("font-size", "0.65rem")
+                    .style("opacity", 0)
+                    .text(d => d)
+                    .transition().duration(750)
+                    .style("opacity", 1);
+            }
+        );
+
+    function update(new_data) {
+    // Joins data to elements, specifying enter, update, exit logic
+        const r_size = 14;
+    
+        let domains = [
+            [d3.min(new_data, d => { return d.center_x }) - r_size, d3.max(new_data, d => { return d.center_x }) + r_size], 
+            [d3.min(new_data, d => { return d.center_y }) - r_size, d3.max(new_data, d => { return d.center_y }) + r_size],
+        ];
+
+        let scale = (domains[1][1]-domains[1][0]) / (domains[0][1]-domains[0][0]);
+
+        let range = [
+            [0, state.dims.width], 
+            [state.dims.height - (state.dims.height * ((1 - scale) / 2)), (state.dims.height * ((1 - scale) / 2))]
+        ];
+
+        xScale.domain(domains[0])
+            .range(range[0]);
+            
+        yScale.domain(domains[1])
+            .range(range[1]);
+
+        svg.append("g")
+            .attr("class", "circles")
+            .selectAll()
+            .data(new_data)
+            .join(
+                (enter) => {
+                    const pack = d3.pack().size([r_size * 1.5 * 2,r_size * 1.5 * 2]).padding(0);
+
+                    const circle_pack = enter.append("g")
+                        .attr("id", d => d.Abb)
+                        .attr("class", "packed-circle")
+                        .style("fill-opacity", 0.6)
+                        .attr("transform", d => `translate(${xScale(d.center_x) - (r_size * 1.5)},${yScale(d.center_y) - (r_size * 1.5)})`)
+                        .on("mouseover", mouseover)
+                        .on("mouseleave", mouseleave);
+                    
+                    const nodes = circle_pack.selectAll()
+                        .data(d => pack(d3.hierarchy({children: d.data}).sum(dd => dd.val)).leaves())
+                        .join("g")
+                        .attr("transform", dd => `translate(${dd.x},${dd.y})`);
+        
+                    nodes.append("circle")
+                        .style("stroke", "gray")
+                        .style("stroke-width", 0.5)
+                        .style("fill", dd => colorScale(dd.data.loc))
+                        .transition().duration(750)
+                        .attr("r", dd => dd.r);
+                        
+                    enter.append("circle")                    
+                        .style("stroke", "gray")
+                        .style("stroke-width", 1)
+                        .style("fill", "none")
+                        .attr("cx", d => xScale(d.center_x))
+                        .attr("cy", d => yScale(d.center_y))
+                        .transition().duration(750)
+                        .attr("r", r_size * 1.5);
+
+                    enter.append("text")
+                        .style("text-anchor", "middle")
+                        .style("font-size", "0.7rem")
+                        .style("opacity", 0)
+                        .attr("x", d => xScale(d.center_x))
+                        .attr("y", d => yScale(d.center_y) - (r_size * 1.7))
+                        .text(d => d.Abb)
+                        .transition().duration(750)
+                        .style("opacity", 1)
+                }, 
+                (update) => update, 
+                (exit) => exit.remove()
+            )
+
+    }
+
+    // Return the update function to be called later
+    return update;
+}
+  
+// Create an instance of the visualization
+const vis = createVis();
+
+// Update the application based on new data via event handlers
+function updateApp() {
+    // Generate new data from updated parameters
+    const wrangled = wrangleData();
+    const filtered = filterData(wrangled);
+
+    // Update visualization
+    vis(filtered);
+}
+
+
+// Load data files
+d3.csv('../../../Data files/nba-team-shooting.csv')
+    .then((shooting) => {
+        d3.csv('../../../Data files/nba-team-stats_2023.csv')
+            .then((shapes) => {
+                // The 'data' variable contains an array of data from all the CSV files
+                // You can process and visualize the data here
+                state.shapes_data = shapes;
+                state.shooting_data = shooting;
+
+                const wrangled = wrangleData();
+                const filtered = filterData(wrangled);
+
+                vis(filtered);
+            })
+            .catch((error) => {
+                console.error('Error loading shape file', error);
+            });    
+    })
+    .catch((error) => {
+        console.error('Error loading shooting file', error);
+    });
