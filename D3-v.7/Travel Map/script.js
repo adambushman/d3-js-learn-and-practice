0db2e142d523129aac3d615b73bd2205b4bddb97
adambushman <adambushman@mail.weber.edu>
2024-07-21 12:11:08 -0600

    Updated Arquero CDN link

--- D3-v.7/Travel Map/script.js
+++ D3-v.7/Travel Map/script.js
@@ -157,7 +157,7 @@ const populateViz = () => {
                     fill_rule: "evenodd"
                 });
                 icon_name = "Tour";
-                desc = "Multiple visits & stays throughout the state.";
+                desc = "Multiple visits/stays throughout the state.";
             } else if(i == 3) {
                 paths.push({
                     d: "M7.293 1.5a1 1 0 0 1 1.414 0L11 3.793V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v3.293l2.354 2.353a.5.5 0 0 1-.708.707L8 2.207 1.354 8.853a.5.5 0 1 1-.708-.707z"
@@ -166,7 +166,7 @@ const populateViz = () => {
                     d: "m14 9.293-6-6-6 6V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5zm-6-.811c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.691 0-5.018"
                 });
                 icon_name = "Live";
-                desc = "Multi-week stays in homes & neighborhoods.";
+                desc = "Multi-week stays in homes/neighborhoods.";
             }
 
             return {x, y, col: (badges[i][1] ? col : "#E2E2E2"), icon_name, desc, paths}
